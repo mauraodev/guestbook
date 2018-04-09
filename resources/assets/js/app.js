2514d004ed9d8a659c35d08ed48dc4a9b03cc615
Mauro Nunes <mauraoemau@hotmail.com>
2018-04-09 17:27:20 -0300

    Commit inicial

---
+++ resources/assets/js/app.js
@@ -0,0 +1,18 @@
+
+/**
+ * First, we will load all of this project's Javascript utilities and other
+ * dependencies. Then, we will be ready to develop a robust and powerful
+ * application frontend using useful Laravel and JavaScript libraries.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.component('signatures', require('./components/Signatures.vue'));
+//Vue.component('signature-form', require('./components/SignatureForm.vue'));
+Vue.component('paginate', require('vuejs-paginate'));
+
+const app = new Vue({
+    el: '#app'
+});
